717557ff3e861c29a2fd118215bd6b8c6fe5b3f4
Loic Mathieu <l.mathieu@kiabi.com>
2014-03-20 13:42:18 +0100

    provide 'scale to fit' zoom

--- src/main/java/com/tagtraum/perf/gcviewer/action/Zoom.java
+++ src/main/java/com/tagtraum/perf/gcviewer/action/Zoom.java
@@ -8,6 +8,8 @@ import javax.swing.AbstractAction;
 import com.tagtraum.perf.gcviewer.GCViewerGui;
 import com.tagtraum.perf.gcviewer.util.LocalisationHelper;
 
+import fr.loicmathieu.gcviewer.SimpleChart;
+
 /**
  *
  * @author <a href="mailto:hs@tagtraum.com">Hendrik Schreiber</a>
@@ -16,29 +18,50 @@ import com.tagtraum.perf.gcviewer.util.LocalisationHelper;
  *
  */
 public class Zoom extends AbstractAction {
-    private GCViewerGui gcViewer;
+	private GCViewerGui gcViewer;
 
-    public Zoom(final GCViewerGui gcViewer) {
-        this.gcViewer = gcViewer;
-        putValue(NAME, LocalisationHelper.getString("action_zoom"));
-        putValue(SHORT_DESCRIPTION, LocalisationHelper.getString("action_zoom_hint"));
-        //putValue(Action.MNEMONIC_KEY, new Integer(LocalisationHelper.getString("main_frame_menuitem_mnemonic_export").charAt(0)));
-        putValue(ACTION_COMMAND_KEY, "zoom");
-        //putValue(Action.ACCELERATOR_KEY, KeyStroke.getKeyStroke('E', Toolkit.getDefaultToolkit().getMenuShortcutKeyMask() ));
-        setEnabled(false);
-    }
+	public Zoom(final GCViewerGui gcViewer) {
+		this.gcViewer = gcViewer;
+		putValue(NAME, LocalisationHelper.getString("action_zoom"));
+		putValue(SHORT_DESCRIPTION, LocalisationHelper.getString("action_zoom_hint"));
+		//putValue(Action.MNEMONIC_KEY, new Integer(LocalisationHelper.getString("main_frame_menuitem_mnemonic_export").charAt(0)));
+		putValue(ACTION_COMMAND_KEY, "zoom");
+		//putValue(Action.ACCELERATOR_KEY, KeyStroke.getKeyStroke('E', Toolkit.getDefaultToolkit().getMenuShortcutKeyMask() ));
+		setEnabled(false);
+	}
 
-    public void actionPerformed(final ActionEvent e) {
-        final ItemSelectable is = (ItemSelectable)e.getSource();
-        final Object[] o = is.getSelectedObjects();
-        try {
-            String item = (String)o[0];
-            if (item.endsWith("%")) item = item.substring(0, item.length()-1);
-            final double zoomFactor = Double.parseDouble(item.trim());
-            if (zoomFactor > 0) gcViewer.getSelectedGCDocument().getModelChart().setScaleFactor(zoomFactor/1000.0);
-        }
-        catch (NumberFormatException nfe) {
-            //nfe.printStackTrace();
-        }
-    }
+	public void actionPerformed(final ActionEvent e) {
+		final ItemSelectable is = (ItemSelectable)e.getSource();
+		final Object[] o = is.getSelectedObjects();
+		try {
+			String item = (String)o[0];
+
+			if(item.equals("Scale to fit")){
+				System.out.println("Scale to fit!");
+				gcViewer.getSelectedGCDocument().getModelChart().autoSetScaleFactor();
+				for(SimpleChart chart : gcViewer.getSelectedGCDocument().getCharts()){
+					chart.autoSetScaleFactor();
+				}
+			}
+			else {
+				if (item.endsWith("%")) {
+					item = item.substring(0, item.length()-1);
+				}
+				final double zoomFactor = Double.parseDouble(item.trim());
+				if (zoomFactor > 0) {
+					//LMA all charts need to listen to zoom event
+					double scaleFactor = zoomFactor/1000.0;
+					gcViewer.getSelectedGCDocument().getModelChart().setScaleFactor(scaleFactor);
+					for(SimpleChart chart : gcViewer.getSelectedGCDocument().getCharts()){
+						chart.setScaleFactor(scaleFactor);
+					}
+				}
+			}
+		}
+		catch (NumberFormatException nfe) {
+			//nfe.printStackTrace();
+		}
+	}
+
+
 }
